6f0563b5076c1cb9403bfcf716cb02a522d957ee
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2018-01-10 22:10:42 -0500

    Handle rate limit of API using caching

--- i3weather.sh
+++ i3weather.sh
@@ -4,7 +4,6 @@
 i3status | (read line && echo "$line" && read line && echo "$line" && read line && echo "$line" && while :
 do
   read line
-  # temp=$(curl -s wttr.in?T | grep -m 1 -Eo -e '-?[[:digit:]].*°C')
-  temp=$(inxi -w | sed -n "s/^.*(\s*\(\S*\).*$/\1°C/p")
+  temp=$(cat ~/.weather.cache | grep -m 1 -Eo -e '-?[[:digit:]].*°C')
   echo ",[{\"full_text\":\" ${temp}\",\"color\":\"#00FF00\" },${line#,\[}" || exit 1
 done)
